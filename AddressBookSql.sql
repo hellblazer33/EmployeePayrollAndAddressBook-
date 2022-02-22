b0e8033508e86782d14235d4c9d6c5a02aa38551
Pankaj Kumar <pk110495@gmail.com>
2022-02-22 07:36:51 +0530

    Create AddressBookSql.sql

---
+++ AddressBookSql.sql
@@ -0,0 +1,25 @@
+create database Address_Book
+
+create table address_book
+(
+id int primary key identity(1,1),
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+address varchar(50),
+city varchar(15),
+state varchar(15),
+zip varchar(6),
+phone_number varchar(15),
+email varchar(50)
+)
+
+
+insert into address_book values
+('Ramesh','Kumar','India','Delhi','Delhi','110001','9384747477','ramesh23@gmail.com'),
+('Aman','Singh','India','Lucknow','UP','400765','9686474221','aman421@gmail.com')
+
+
+update address_book set state='MP' where first_name='Aman'
+
+
+select * from address_book where city='Delhi'
